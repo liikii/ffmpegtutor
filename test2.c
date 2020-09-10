61dd91239dc6914a75fe80150c06d458a11474f9
liikii <609957891@qq.com>
2020-09-10 19:50:17 +0800

    u

---
+++ test2.c
@@ -0,0 +1,14 @@
+// 将字符串复制到数组 dest 中
+#include <stdio.h>
+#include <string.h>
+ 
+int main ()
+{
+   const char src[50] = "http://www.runoob.com";
+   char dest[50] = "abc";
+ 
+   memcpy(dest, src, strlen(src)+1);
+   printf("dest = %s\n", dest);
+   
+   return(0);
+}
