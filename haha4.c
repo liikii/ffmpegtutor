c6d4e40e808f0c1740ade206329ebd107f519893
liikii <609957891@qq.com>
2020-09-14 11:40:04 +0800

    u

--- haha4.c
+++ haha4.c
@@ -37,9 +37,106 @@
 #define VIDEO_PICTURE_QUEUE_SIZE 1
 
 
+typedef struct PacketQueue {
+  AVPacketList *first_pkt, *last_pkt;
+  int nb_packets;
+  int size;
+  SDL_mutex *mutex;
+  SDL_cond *cond;
+} PacketQueue;
+
+
+typedef struct VideoPicture {
+  SDL_Overlay *bmp;
+  int width, height; /* source height & width */
+  int allocated;
+} VideoPicture;
+
+
+typedef struct VideoState {
+  AVFormatContext *pFormatCtx;
+  int             videoStream, audioStream;
+  AVStream        *audio_st;
+  AVCodecContext  *audio_ctx;
+  PacketQueue     audioq;
+  uint8_t         audio_buf[(AVCODEC_MAX_AUDIO_FRAME_SIZE * 3) / 2];
+  unsigned int    audio_buf_size;
+  unsigned int    audio_buf_index;
+  AVFrame         audio_frame;
+  AVPacket        audio_pkt;
+  uint8_t         *audio_pkt_data;
+  int             audio_pkt_size;
+  AVStream        *video_st;
+  AVCodecContext  *video_ctx;
+  PacketQueue     videoq;
+  struct SwsContext *sws_ctx;
+
+  VideoPicture    pictq[VIDEO_PICTURE_QUEUE_SIZE];
+  int             pictq_size, pictq_rindex, pictq_windex;
+  SDL_mutex       *pictq_mutex;
+  SDL_cond        *pictq_cond;
+  
+  SDL_Thread      *parse_tid;
+  SDL_Thread      *video_tid;
+
+  char            filename[1024];
+  int             quit;
+} VideoState;
+
+
+
+/* Since we only have one decoding thread, the Big Struct
+   can be global in case we need it. */
+VideoState *global_video_state;
+
+
+void packet_queue_init(PacketQueue *q) {
+    //  初始化.  
+    memset(q, 0, sizeof(PacketQueue));
+    q->mutex = SDL_CreateMutex();
+    q->cond = SDL_CreateCond();
+}
+
+
+
+int packet_queue_put(PacketQueue *q, AVPacket *pkt) {
+    AVPacketList *pkt1;
+    // 复制原因不明. 
+    if(av_dup_packet(pkt) < 0) {
+        return -1;
+    }
+    // 
+    pkt1 = av_malloc(sizeof(AVPacketList));
+    if (!pkt1){
+        return -1;
+    }
+    pkt1->pkt = *pkt;
+    // 因为是新的. next为null
+    pkt1->next = NULL;
+
+    // 
+    SDL_LockMutex(q->mutex);
+
+    if (!q->last_pkt){
+        // 第一. 
+        q->first_pkt = pkt1;
+    }else{
+        q->last_pkt->next = pkt1;
+    }
+    q->last_pkt = pkt1;
+    q->nb_packets++;
+    q->size += pkt1->pkt.size;
+
+    SDL_CondSignal(q->cond);
+    SDL_UnlockMutex(q->mutex);
+    return 0;
+}
+
+
+
 int main(int argc, char const *argv[])
 {
     /* code */
-    printf("%s\n", "");
+    printf("%s\n", "hello ");
     return 0;
 }
